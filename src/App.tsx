579fc6fd21fc2456a46fe4f7732a6795f93618fb
Michael-Crowther <67437978+Michael-Crowther@users.noreply.github.com>
2023-04-11 10:41:44 -0600

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -92,7 +92,7 @@ const App: React.FC = () => {
       const loading = document.querySelector(".docHidden");
 
       try{
-        const response = await fetch('http://localhost:5000/login', {
+        const response = await fetch('https://qa-qc-bible.com/login', {
           method: 'POST',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({email, password}),
