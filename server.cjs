7724413fa60053df8108be6939d60f53310e6fa7
Michael-Crowther <67437978+Michael-Crowther@users.noreply.github.com>
2023-04-13 18:29:56 -0600

    Add files via upload

--- server.cjs
+++ server.cjs
@@ -265,14 +265,14 @@ app.post('/request-term', async (req, res) => {
 
 
 // Serve the combined JavaScript file
-app.use(express.static(path.join(__dirname, './dist')));
+app.use(express.static(path.join(__dirname, '/dist')));
 
 // Serve the index.html file for all other requests
-/*
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
-*/
+
 //Start the server
 app.listen(port, () => {
   console.log("Server started on port", port);
