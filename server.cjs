d934d1c5f56217be2fc9e13826065d283a51c459
Michael-Crowther <67437978+Michael-Crowther@users.noreply.github.com>
2023-04-15 12:13:42 -0600

    Add files via upload

--- server.cjs
+++ server.cjs
@@ -25,12 +25,18 @@ app.use(bodyParser.json());
 
 //setup postgreSQL pool
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
+
+pool.connect();
 
 const jwtsecret = process.env.JWT_SECRET;
 
